5c6c8c3c6104453019bc04c65d830a79f48fe1f2
Giovanni Davì <giovanni.davi@yahoo.com>
2025-12-31 10:22:27 +0100

    fix basepath

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/chatbot/', // Sostituisci con il nome del tuo repository
+  base: '/orologio-rubato/', // Sostituisci con il nome del tuo repository
 })
